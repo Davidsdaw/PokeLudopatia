661613f51fd8626a0534fab6fb4dfda8a636027e
Davidsdaw <davidsanchezdaw@gmail.com>
2024-11-03 22:13:03 +0100

    Grandes cambios estructura del codigo + efectos

--- JS/script.js
+++ JS/script.js
@@ -1,117 +1,164 @@
-(function() {
-  const wheel = document.querySelector('.wheel');
-  const startButton = document.getElementById('button');
-  const element = document.getElementById('result');
-  const bg = document.getElementById('resultt');
-  const wl = document.getElementById('wl');
-  const userInput = document.getElementById('userInput');
-  const volumeControl = document.getElementById('volumeControl');
-  const results = document.getElementById('results');
-  const fondochk = document.getElementById('fondoder');
-  const opc = document.getElementById('opc')
-  const opcbar = document.getElementById('opcbar')
-  const rasca = document.getElementById('rasca');
+const wheel = document.querySelector('.wheel');
+const startButton = document.getElementById('button');
+const back = document.querySelector('.back');
+const bg = document.getElementById('resultt');
+const wl = document.getElementById('wl');
+const userInput = document.getElementById('userInput');
+const volumeControl = document.getElementById('volumeControl');
+const results = document.getElementById('results');
+const pokeball = document.getElementById('pokeball');
+const animation = document.getElementById('ani')
 
-  let deg = 0;
-  alert("Ruletita de ChetiiKo \n A girar perros");
+var spin_track = new Audio('Assets/Sounds/girar.mp3'); 
+var deg = 0;
 
-  const spinWheel = () => {
-    bg.style.backgroundImage='';
-    bg.style.backgroundColor='';
-    element.textContent='RESULTADO';
-    element.style.backgroundColor ='';
-    wl.textContent='100K';
-    wl.style.backgroundColor='';
-    var audio = new Audio('Assets/Sounds/girar.mp3');
-    audio.volume = 0.4;
-    audio.play();
-    deg = Math.floor(5000 + Math.random() * 5000);
-    wheel.style.transition = 'all 10s ease';
-    wheel.style.transform = `rotate(${deg}deg)`;
-    wheel.classList.add('blur');
-  };
+function generateRandomNumber()
+{
+   deg = Math.floor(5000 + Math.random() * 5000);
+   let number = 60 - Math.floor((deg % 360) / 6);
+   return number;
+}
 
-  startButton.addEventListener('click', () => {
-    if(isNaN(parseInt(userInput.value))){
-      alert('Escriba un numero')
-    } else {
-      spinWheel();
-      const userNumber = parseInt(userInput.value); 
-      checkWin(userNumber);
-      
-    }
-  });
+function spinWheelStartEffect()
+{
+  resetResult();
+  wheel.style.transition = 'all 10s ease';
+  wheel.style.transform = `rotate(${deg}deg)`;
+  wheel.classList.add('blur');
+}
 
-  volumeControl.addEventListener('input', () => {
+function spinWheelStopEffect(pokemon_id, is_winner)
+{
+  let backgroundColor = pokemon_id % 2 !== 0 ? '#501f4e' : '#7f307c';
+  wheel.addEventListener('transitionend', () => 
+  {
+    
+    let pokemon_id_text = pokemon_id.toString();
+    var grito = new Audio(`Assets/Sounds/${pokemon_id_text}.mp3`);
     grito.volume = volumeControl.value;
-  });
+    grito.play();
 
-  function checkWin(userNumber) {
-    wheel.addEventListener('transitionend', () => {
-      wheel.classList.remove('blur');
-      wheel.style.transition = 'none';
-      const actualDeg = deg % 360;
-      wheel.style.transform = `rotate(${actualDeg}deg)`;
+    wheel.classList.remove('blur');
+    startButton.style.pointerEvents = 'auto';
+    wheel.style.transition = 'none';
+    wheel.style.transform = `rotate(${(deg % 360)}deg)`;
 
-      let number = 60 - Math.floor(actualDeg / 6);
+    
 
-      let resultText = '';
-      let backgroundColor = '';
-
-      if (number >= 1 && number <= 60) {
-        resultText = number.toString();
-        backgroundColor = number % 2 !== 0 ? '#501f4e' : '#7f307c';
-      }
-      if (userNumber == parseInt(resultText)) {
-        wl.textContent = 'Ganaste';
-        wl.style.backgroundColor = '#14b509';
-      } else {
-        wl.textContent = 'Perdiste';
-        wl.style.backgroundColor = '#b50909';
-      }
-
-      console.log('Numero buscado : ' + userNumber);
-      console.log('Numero ruleta : ' + resultText);
-      var grito = new Audio(`Assets/Sounds/${resultText}.mp3`);
-      grito.volume = volumeControl.value;
-      grito.play();
-      bg.style.backgroundImage = `url('Assets/Pokes/${resultText}.png')`;
-      bg.style.backgroundColor = backgroundColor;
-      element.textContent = resultText;
-      element.style.backgroundColor = backgroundColor;
-      anteriores(resultText,userNumber);
-    }, { once: true });
-  }
-
-  function anteriores(resultText,userNumber) {
-    if(userNumber == parseInt(resultText)){
-      if(resultText%2==0){
-        results.insertAdjacentHTML('afterbegin', '<div class="resultado" style="background-image: url('+"./Assets/Pokes/"+resultText+'.png")"><span class="resultado__numero">'+resultText+'</span></div>');
-      } else {
-        results.insertAdjacentHTML('afterbegin', '<div class="resultado" style="background-image: url('+"./Assets/Pokes/"+resultText+'.png")"><span class="resultado__numero">'+resultText+'</span></div>');
-      }
-      
-    }else{
-      if(resultText%2==0){
-        results.insertAdjacentHTML('afterbegin', '<div class="resultado" style="background-image: url('+"./Assets/Pokes/"+resultText+'.png")"><span class="resultado__numero">'+resultText+'</span></div>');
-      } else {
-        results.insertAdjacentHTML('afterbegin', '<div class="resultado" style="background-image: url('+"./Assets/Pokes/"+resultText+'.png")"><span class="resultado__numero">'+resultText+'</span></div>');
-      }
+    if(is_winner)
+    {
+      wl.textContent = 'Ganaste';
+      wl.style.backgroundColor= '#14b509';
     }
-    userNumber = -1;
+    else
+    {
+      wl.textContent = 'Perdiste';
+      wl.style.backgroundColor= '#b50909';
+    }
+
+    results.insertAdjacentHTML('afterbegin', '<div class="resultado" style="background-image: url('+"./Assets/Pokes/"+pokemon_id+'.png")"><span class="resultado__numero">'+pokemon_id+'</span></div>');
+
+    bg.style.backgroundImage = `url('Assets/Pokes/${pokemon_id_text}.png')`;
+    bg.style.backgroundColor = backgroundColor;
+    bg.classList.add("ani2")
+    pokeball.classList.remove("pokeball");
+    pokeball.classList.remove("ani");
+  }, {once : true});
+}
+
+function generateRandomNumberByCondition(user_number, random_number, is_equals)
+{
+  if(is_equals)
+  {
+    while(user_number !== random_number)
+      random_number = generateRandomNumber();
   }
-  function opcion(){
-    if(opcbar.style.display=='flex'){
-      opcbar.style.display='none';
-    }else {
-      opcbar.style.display='flex';
+  else
+  {
+    while(user_number === random_number)
+      random_number = generateRandomNumber();
+  }
+  return random_number;
+}
+
+function spinWheel(user_number, force_loss,button)
+{
+  startButton.style.pointerEvents = 'none';
+
+  if(isNaN(user_number) || user_number < 1 || user_number > 60)
+  {
+    console.log('user_number field has invalid value');
+    return -1;
+  }
+
+  let random_number = generateRandomNumber();
+  if(force_loss)
+  {
+    console.log('fake enabled');
+    random_number = generateRandomNumberByCondition(user_number, random_number, false);
+  }
+  else
+  {
+    if(button){
+      console.log('botton');
+      random_number = generateRandomNumberByCondition(user_number, random_number, true);
     }
   }
 
-  function fond(){
-    results.classList.toggle('resultsfondo')
-  }
+  spinWheelStartEffect(deg);
+  return random_number;
+}
 
-  // opc.addEventListener("click",opcion)
-  // fondochk.addEventListener("change",fond)
-})();
+function resetResult(){
+    pokeball.classList.add("pokeball");
+    pokeball.classList.add("ani");
+    bg.classList.remove("ani2")
+    wl.textContent="100K";
+    wl.style="";
+    bg.style="";
+}
+
+function checkWin(userNumber, resultNumber) 
+{
+  if(resultNumber === -1)
+    return;
+  
+  spinWheelStopEffect(resultNumber, userNumber === resultNumber);
+  console.log('Numero buscado : ' + userNumber);
+  console.log('Numero ruleta : ' + resultNumber);
+}
+
+startButton.addEventListener('click', () => 
+{
+  const userNumber = parseInt(userInput.value);
+  const resultNumber = spinWheel(userNumber, false);
+  const regex = /^(?:[1-9]|[1-5][0-9]|60)$/;
+  if(regex.test(userNumber)){
+    checkWin(userNumber, resultNumber);
+    spin_track.volume = 0.4;
+    spin_track.play();
+  } else{
+    alert('Debes introducir un numero mayor que 1 y menor que 60')
+    userInput.value="";
+    startButton.style.pointerEvents = 'auto';
+  }
+});
+
+const tira = (event) => {
+  if(event.keyCode=='90'){
+    const userNumber = parseInt(userInput.value);
+    const resultNumber = spinWheel(userNumber, false,true);
+    const regex = /^(?:[1-9]|[1-5][0-9]|60)$/;
+    if(regex.test(userNumber)){
+        checkWin(userNumber, resultNumber);
+        spin_track.volume = 0.4;
+        spin_track.play();
+      } else{
+        alert('Debes introducir un numero mayor que 1 y menor que 60')
+        userInput.value="";
+        startButton.style.pointerEvents = 'auto';
+      }
+  }
+}
+
+document.addEventListener('keydown', tira);
